a13611225471ab1342091aeae3ac1129e3bf2145
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-22 11:22:36 +0200

    correction avec la classe

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-$lastNameErr = $firstNameErr = $fileErr = '';
-$lastName = $firstName = $file = '';
-?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
@@ -9,61 +5,52 @@ $lastName = $firstName = $file = '';
         <link rel="stylesheet" href="assets/css/style.css" />
         <title>Exercice 7</title>
     </head>
-    <body><!--Formulaire de qualité-->
-        <?php
-        if ((isset($_POST['lastName']) && !empty($_POST['lastName']) && preg_match('/^[a-zA-Z\- éèêç]*$/', $_POST['lastName'])) && (isset($_POST['firstName']) && !empty($_POST['firstName']) && preg_match('/^[a-zA-Z\- éèêç]*$/', $_POST['firstName'])) && (isset($_POST['choice']) && !empty($_POST['choice'])) && isset($_FILES['phpFile'])) {
-            echo 'Nom: ' . $_POST['lastName'];
-            ?>
-            <br />
-            <?php
-            echo 'Prénom: ' . $_POST['firstName'];
-            ?>
-            <br />
-            <?php
-            echo 'Genre: ' . $_POST['choice'];
-            ?>
-            <br />
-            <?php
-            echo 'Votre fichier: ' . $_FILES['phpFile']['name'];
-        } else {
-            if (empty($_POST['lastName'])) {
-                $lastNameErr = 'Ce champs est vide';
-            } elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $lastName)) {
-                $lastNameErr = 'Votre saisie possède des caractères incorrects';
-            }
-
-            if (empty($_POST['firstName'])) {
-                $firstNameErr = 'Ce champs est vide';
-            } elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $firstName)) {
-                $firstNameErr = 'Votre saisie possède des caractères incorrects';
-            }
-            ?>
-            <form class="form" action="#" method="post" enctype="multipart/form-data"><!--Création formulaire-->
-                <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" /><span class="error">* <?php echo $lastNameErr; ?></span><br><br>
-                <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" /><span class="error">* <?php echo $firstNameErr; ?></span><br><br>
-                <label for="choice" name="choice">Votre sexe: </label>
-                <select class="choice" name="choice">
-                    <option value="monsieur">Monsieur</option>
-                    <option value="madame">Madame</option>
-                    <option value="non_binaire">Non-Binaire</option>
-                </select><br><br>
-                <label for="phpFile" name="phpFile">Uploadez ici votre fichier: </label>
-                <input type="file" name="phpFile" /><span class="error">*<?php echo $fileErr; ?></span><br><br>
-                <input type="submit" name="submit" />
-            </form>
-            <?php
-            if (empty($_POST['lastName'])) {
-                $lastNameErr = 'Ce champs est vide';
-            } elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $lastName)) {
-                $lastNameErr = 'Votre saisie possède des caractères incorrects';
-            }
-
-            if (empty($_POST['firstName'])) {
-                $firstNameErr = 'Ce champs est vide';
-            } elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $firstName)) {
-                $firstNameErr = 'Votre saisie possède des caractères incorrects';
-            }
-        }
+    <body><!--Condition pour afficher les informations dans une phrase complète si toutes les informations sont renseignées-->
+    <?php
+    if(isset($_POST['submit'])){
+      if(!empty($_POST['lastName']) && !empty($_POST['firstName']) && !empty($_POST['civility']) && !empty($_FILES['file'])){
+        $fileInfo = pathinfo($_FILES['file']['name']);
+        var_dump($fileInfo);
+        echo 'Je m\'appelle ' . $_POST['firstName'] . ' ' . $_POST['lastName'] . ' et je suis ' . $_POST['civility'] . ' et voici mon CV: ' . $_FILES['file']['name'];
+      } else {
         ?>
-    </body>
+        <form action="#" method="POST" enctype="multipart/form-data"><!--Création formulaire qui ne s'affiche qu'au début et lorsque toutes les informations ne sont pas remplies-->
+          <label for="lastName">Votre nom: </label><br>
+          <input type="text" name="lastName" id="lastName" placeholder="nom" size="30" /><!--Champs nom de famille-->
+          <label for="firstName">Votre prénom: </label><br>
+          <input type="text" name="firstName" id="firstName" placeholder="prénom" size="30" /><!--Champs prénom-->
+          <select name="civility" id="civility"><!--Champs de sélection du sexe-->
+            <option selected disabled>Sélectionner votre sexe</option>
+            <option value="un homme">Masculin</option><!--Selection masculin-->
+            <option value="une femme">Féminin</option><!--Selection féminin-->
+            <option value="non_binaire">Non Binaire</option><!--Selection non-binaire-->
+          </select><br>
+          <label for="file">Sélectionnez un fichier: </label> 
+          <input type="file" name="file" /><br>
+          <input type="submit" name="submit" id="submit" value="Soumettre" /><span class="error"><?= 'Tous les champs ne sont pas remplis !';?></span>
+        </form>
+        <?php
+      }
+  } else {
+?>
+    <form action="#" method="POST" enctype="multipart/form-data"><!--Création formulaire qui ne s'affiche qu'au début et lorsque toutes les informations ne sont pas remplies-->
+      <label for="lastName">Votre nom: </label>
+      <input type="text" name="lastName" id="lastName" placeholder="nom" size="30" /><br><!--Champs nom de famille-->
+      <label for="firstName">Votre prénom: </label>
+      <input type="text" name="firstName" id="firstName" placeholder="prénom" size="30" /><br><!--Champs prénom-->
+      <label for="civility">Votre sexe: </label>
+      <select name="civility" id="civility"><!--Champs de sélection du sexe-->
+        <option selected disabled>Sélectionner votre sexe</option>
+        <option value="un homme">Masculin</option><!--Selection masculin-->
+        <option value="une femme">Féminin</option><!--Selection féminin-->
+        <option value="non_binaire">Non Binaire</option><!--Selection non-binaire-->
+      </select><br>
+      <label for="file">Sélectionnez un fichier: </label> 
+      <input type="file" name="file" /><br>
+      <input type="submit" name="submit" value="Soumettre" />
+    </form>
+    <?php
+  }
+  ?>
+  </body>
 </html>
